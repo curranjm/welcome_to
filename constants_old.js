6b1eb6fef5db8224fc92be4d3ff4ae38cf303912
curranjm <curran.jm@gmail.com>
2019-05-11 18:09:11 -0400

    added old constants file

---
+++ constants_old.js
@@ -0,0 +1,409 @@
+module.exports = {
+  CARDS: Object.freeze([
+    {
+      cardID: 1,
+      value: 1,
+      action: 16
+    },
+    {
+      cardID: 8,
+      value: 1,
+      action: 17
+    },
+    {
+      cardID: 8,
+      value: 1,
+      action: 18
+    },
+    {
+      cardID: 8,
+      value: 2,
+      action: 16
+    },
+    {
+      cardID: 8,
+      value: 2,
+      action: 17
+    },
+    {
+      cardID: 8,
+      value: 2,
+      action: 18
+    },
+    {
+      cardID: 8,
+      value: 3,
+      action: 16
+    },
+    {
+      cardID: 8,
+      value: 3,
+      action: 21
+    },
+    {
+      cardID: 9,
+      value: 3,
+      action: 20
+    },
+    {
+      cardID: 10,
+      value: 3,
+      action: 19
+    },
+    {
+      cardID: 11,
+      value: 4,
+      action: 19
+    },
+    {
+      cardID: 12,
+      value: 4,
+      action: 21
+    },
+    {
+      cardID: 13,
+      value: 4,
+      action: 18
+    },
+    {
+      cardID: 14,
+      value: 4,
+      action: 20
+    },
+    {
+      cardID: 15,
+      value: 4,
+      action: 17
+    },
+    {
+      cardID: 16,
+      value: 5,
+      action: 18
+    },
+    {
+      cardID: 17,
+      value: 5,
+      action: 16
+    },
+    {
+      cardID: 18,
+      value: 5,
+      action: 17
+    },
+    {
+      cardID: 19,
+      value: 5,
+      action: 18
+    },
+    {
+      cardID: 20,
+      value: 5,
+      action: 17
+    },
+    {
+      cardID: 21,
+      value: 5,
+      action: 16
+    },
+    {
+      cardID: 22,
+      value: 6,
+      action: 20
+    },
+    {
+      cardID: 23,
+      value: 6,
+      action: 19
+    },
+    {
+      cardID: 24,
+      value: 6,
+      action: 17
+    },
+    {
+      cardID: 25,
+      value: 6,
+      action: 16
+    },
+    {
+      cardID: 26,
+      value: 6,
+      action: 16
+    },
+    {
+      cardID: 27,
+      value: 6,
+      action: 18
+    },
+    {
+      cardID: 28,
+      value: 6,
+      action: 21
+    },
+    {
+      cardID: 29,
+      value: 7,
+      action: 21
+    },
+    {
+      cardID: 30,
+      value: 7,
+      action: 18
+    },
+    {
+      cardID: 31,
+      value: 7,
+      action: 16
+    },
+    {
+      cardID: 32,
+      value: 7,
+      action: 18
+    },
+    {
+      cardID: 33,
+      value: 7,
+      action: 17
+    },
+    {
+      cardID: 34,
+      value: 7,
+      action: 20
+    },
+    {
+      cardID: 35,
+      value: 7,
+      action: 17
+    },
+    {
+      cardID: 36,
+      value: 7,
+      action: 19
+    },
+    {
+      cardID: 37,
+      value: 8,
+      action: 18
+    },
+    {
+      cardID: 38,
+      value: 8,
+      action: 20
+    },
+    {
+      cardID: 39,
+      value: 8,
+      action: 16
+    },
+    {
+      cardID: 40,
+      value: 8,
+      action: 19
+    },
+    {
+      cardID: 41,
+      value: 8,
+      action: 17
+    },
+    {
+      cardID: 42,
+      value: 8,
+      action: 16
+    },
+    {
+      cardID: 43,
+      value: 8,
+      action: 18
+    },
+    {
+      cardID: 44,
+      value: 8,
+      action: 21
+    },
+    {
+      cardID: 45,
+      value: 8,
+      action: 17
+    },
+    {
+      cardID: 46,
+      value: 9,
+      action: 18
+    },
+    {
+      cardID: 47,
+      value: 9,
+      action: 20
+    },
+    {
+      cardID: 48,
+      value: 9,
+      action: 21
+    },
+    {
+      cardID: 49,
+      value: 9,
+      action: 19
+    },
+    {
+      cardID: 50,
+      value: 9,
+      action: 18
+    },
+    {
+      cardID: 51,
+      value: 9,
+      action: 17
+    },
+    {
+      cardID: 52,
+      value: 9,
+      action: 16
+    },
+    {
+      cardID: 53,
+      value: 9,
+      action: 17
+    },
+    {
+      cardID: 54,
+      value: 10,
+      action: 16
+    },
+    {
+      cardID: 55,
+      value: 10,
+      action: 19
+    },
+    {
+      cardID: 56,
+      value: 10,
+      action: 18
+    },
+    {
+      cardID: 57,
+      value: 10,
+      action: 20
+    },
+    {
+      cardID: 58,
+      value: 10,
+      action: 21
+    },
+    {
+      cardID: 59,
+      value: 10,
+      action: 17
+    },
+    {
+      cardID: 60,
+      value: 10,
+      action: 16
+    },
+    {
+      cardID: 61,
+      value: 11,
+      action: 18
+    },
+    {
+      cardID: 62,
+      value: 11,
+      action: 16
+    },
+    {
+      cardID: 63,
+      value: 11,
+      action: 17
+    },
+    {
+      cardID: 64,
+      value: 11,
+      action: 16
+    },
+    {
+      cardID: 65,
+      value: 11,
+      action: 17
+    },
+    {
+      cardID: 66,
+      value: 11,
+      action: 18
+    },
+    {
+      cardID: 67,
+      value: 12,
+      action: 18
+    },
+    {
+      cardID: 68,
+      value: 12,
+      action: 21
+    },
+    {
+      cardID: 69,
+      value: 12,
+      action: 19
+    },
+    {
+      cardID: 70,
+      value: 12,
+      action: 17
+    },
+    {
+      cardID: 71,
+      value: 12,
+      action: 20
+    },
+    {
+      cardID: 72,
+      value: 13,
+      action: 20
+    },
+    {
+      cardID: 73,
+      value: 13,
+      action: 19
+    },
+    {
+      cardID: 74,
+      value: 13,
+      action: 16
+    },
+    {
+      cardID: 75,
+      value: 13,
+      action: 21
+    },
+    {
+      cardID: 76,
+      value: 14,
+      action: 18
+    },
+    {
+      cardID: 77,
+      value: 14,
+      action: 17
+    },
+    {
+      cardID: 78,
+      value: 14,
+      action: 16
+    },
+    {
+      cardID: 79,
+      value: 15,
+      action: 18
+    },
+    {
+      cardID: 80,
+      value: 15,
+      action: 17
+    },
+    {
+      cardID: 81,
+      value: 15,
+      action: 16
+    }
+  ])
+};
